598039ed940f52da3cdce89106fb793ce0624a8a
arnaud.gregoire <arnaud.gregoire@ptvgroup.com>
2019-05-27 17:30:40 +0200

    sprites work

--- public/js/game.js
+++ public/js/game.js
@@ -17,12 +17,14 @@ function preload() {
   this.load.image('otherPlayer', 'assets/enemyBlack5.png');
   this.load.image('tiles','assets/test_tileset.png');
   this.load.tilemapTiledJSON('map','assets/test_map.json');
+  this.load.multiatlas('sprites','assets/test_sprites.json');
 }
  
 function create() {
   var self = this;
   this.socket = io();
   this.players = this.add.group();
+  createAnimations(self);
 
   const map = this.make.tilemap({key:'map'});
   const tileset = map.addTilesetImage('test_tileset', 'tiles');
@@ -97,8 +99,22 @@ function update() {
 
 function displayPlayers(self, playerInfo, sprite) {
   const player = self.add.sprite(playerInfo.x, playerInfo.y, sprite).setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  //player.anims.play('sprites');
   if (playerInfo.team === 'blue') player.setTint(0x0000ff);
   else player.setTint(0xff0000);
   player.playerId = playerInfo.playerId;
   self.players.add(player);
 }
+
+function createAnimations(self) {
+  //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+  let frameNames = self.anims.generateFrameNames('sprites', {
+      start:1,
+      end:8,
+      prefix:'sprite'
+    });
+
+  self.anims.create({ key: 'test', frames: frameNames, frameRate: 10, repeat: -1 });
+
+}
+ 
