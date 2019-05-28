4bed42c9e35e4494e3349e863e3c94cd29690ff7
arnaudgregoire <arnaudgregoire92@gmail.com>
2019-05-28 21:44:59 +0200

    modif server side

--- server/js/game.js
+++ server/js/game.js
@@ -33,16 +33,16 @@ function create() {
     console.log('a user connected');
     // create a new player and add it to our players object
     players[socket.id] = {
-      rotation: 0,
       x: Math.floor(Math.random() * 700) + 50,
       y: Math.floor(Math.random() * 500) + 50,
       playerId: socket.id,
-      team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue',
       input: {
         left: false,
         right: false,
-        up: false
-      }
+        up: false,
+        down: false
+      },
+      sprite: 'left'
     };
     // add player to server
     addPlayer(self, players[socket.id]);
@@ -70,42 +70,45 @@ function create() {
  
 function update() {
   this.players.getChildren().forEach((player) => {
+    player.setVelocity(0);
     const input = players[player.playerId].input;
     if (input.left) {
-      player.setAngularVelocity(-300);
-    } else if (input.right) {
-      player.setAngularVelocity(300);
-    } else {
-      player.setAngularVelocity(0);
+      player.setVelocityX(-80);
+      players[player.playerId].sprite = 'left';
+    } 
+    if (input.right) {
+      player.setVelocityX(80);
+      players[player.playerId].sprite = 'right';
+    } 
+    if (input.down) {
+      player.setVelocityY(80);
+      players[player.playerId].sprite = 'down';
     }
-  
     if (input.up) {
-      this.physics.velocityFromRotation(player.rotation + 1.5, 200, player.body.acceleration);
-    } else {
-      player.setAcceleration(0);
+      player.setVelocityY(-80);
+      players[player.playerId].sprite = 'up';
     }
   
     players[player.playerId].x = player.x;
     players[player.playerId].y = player.y;
-    players[player.playerId].rotation = player.rotation;
   });
   this.physics.world.wrap(this.players, 5);
   io.emit('playerUpdates', players);
 }
 
 function handlePlayerInput(self, playerId, input) {
+  console.log(input);
   self.players.getChildren().forEach((player) => {
     if (playerId === player.playerId) {
       players[player.playerId].input = input;
     }
   });
+
+  
 }
 
 function addPlayer(self, playerInfo) {
-  const player = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
-  player.setDrag(100);
-  player.setAngularDrag(100);
-  player.setMaxVelocity(200);
+  const player = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship');
   player.playerId = playerInfo.playerId;
   self.players.add(player);
 }
