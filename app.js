dbc4517d7bb901681f17d7158a9e331908c6aa10
arnaudgregoire <arnaudgregoire92@gmail.com>
2019-06-08 22:14:43 +0200

    stable version

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-<<<<<<< HEAD
 const ServerController = require('./serverController');
 let serverController = new ServerController();
 serverController.initialize();
-serverController.addGameController().then(function () {
+/*serverController.addGameController().then(function () {
   console.log("second game Controller deployed");
-});
+});*/
 
-=======
-
-
-/*
-app.post('/submit-chatline', passport.authenticate('jwt', { session : false }), asyncMiddleware(async (req, res, next) => {
-  const { message } = req.body;
-  const { email, name } = req.user;
-  // await ChatModel.create({ email, message });
-  io.emit('new-message', {
-    username: name,
-    message,
-  });
-  res.status(200).json({ status: 'ok' });
-}));
-*/
-
-
-const ServerController = require('./serverController');
-let serverController = new ServerController();
-serverController.initialize();
-
->>>>>>> e824b38b01ef8e0291708b4ec52d323439bd92ce
 
--- serverController.js
+++ serverController.js
@@ -11,10 +11,6 @@ const defaultExport = require('./generation/generationBenchmark');
 const routes = require('./routes/main');
 const secureRoutes = require('./routes/secure');
 const express = require('express');
-<<<<<<< HEAD
-const asyncMiddleware = require('./middleware/asyncMiddleware');
-=======
->>>>>>> e824b38b01ef8e0291708b4ec52d323439bd92ce
 
 
 class ServerController{
@@ -87,17 +83,10 @@ class ServerController{
     }
 
     intializeRoutes(){
-<<<<<<< HEAD
         let self = this;
         // require passport auth
         require('./auth/auth');
         this.app.use(express.static(__dirname));
-=======
-        // require passport auth
-        require('./auth/auth');
-        this.app.use(express.static(__dirname));
-        console.log(__dirname);
->>>>>>> e824b38b01ef8e0291708b4ec52d323439bd92ce
         this.app.get('/generation', function (req, res) {
             defaultExport().then(
               () => {
@@ -113,7 +102,6 @@ class ServerController{
         this.app.get('/index.html', function (req, res) {
             res.sendFile(__dirname + '/public/index.html');
         });
-<<<<<<< HEAD
 
         this.app.post('/submit-chatline', passport.authenticate('jwt', { session : true }), function (req, res){
             const { message } = req.body;
@@ -129,8 +117,6 @@ class ServerController{
             res.status(200).json({ status: 'ok' });
         });
 
-=======
->>>>>>> e824b38b01ef8e0291708b4ec52d323439bd92ce
         // main routes
         this.app.use('/', routes);
         this.app.use('/', passport.authenticate('jwt', { session : true }), secureRoutes);
@@ -146,30 +132,18 @@ class ServerController{
             res.status(err.status || 500).json({ error: err.message });
         });
     }
-<<<<<<< HEAD
     
-=======
-
->>>>>>> e824b38b01ef8e0291708b4ec52d323439bd92ce
     addGameController(){
         let self = this;
         return new Promise(
             function (resolve, reject) {
-<<<<<<< HEAD
                 let io = require('socket.io').listen(self.server);
-=======
-                const io = require('socket.io').listen(self.server);
->>>>>>> e824b38b01ef8e0291708b4ec52d323439bd92ce
                 io.use(ios(self.session));
                 // link between socket.io and express session
                 let gameController = new GameController(io);
                 gameController.initialize()
                 .then(()=>{
-<<<<<<< HEAD
                     self.gameControllers.push(gameController);
-=======
-                    self.gameControllers += [gameController];
->>>>>>> e824b38b01ef8e0291708b4ec52d323439bd92ce
                     resolve();
                 })
                 .catch((error) => {reject(error)})
