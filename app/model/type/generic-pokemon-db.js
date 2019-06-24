779f7790a995d048870b61d7987e8566cbd6d943
arnaud.gregoire <arnaud.gregoire@ptvgroup.com>
2019-06-24 11:58:34 +0200

    add generic pokemon db constructor

--- app/model/type/generic-pokemon-db.js
+++ app/model/type/generic-pokemon-db.js
@@ -1,5 +1,43 @@
+const Ability = require('./ability');
+const Move = require('./move');
+const Stat = require('./stat');
+const Type = require('./type');
+
 class GenericPokemonDB{
     constructor(doc){
-        
+        let self = this;
+        self.abilities = [];
+        doc.abilities.forEach(ability => {
+            self.abilities.push({
+                ability: new Ability(ability.ability),
+                is_hidden: ability.is_hidden,
+                slot: ability.slot
+            })
+        });
+        self.baseExperience = doc.base_experience;
+        self.moves = [];
+        doc.moves.forEach((move) =>{
+            self.moves.push(new Move(
+                mmove.name,
+                move.url,
+                move.levelLearnedAt,
+                move.move_learn_method                
+            ))
+        });
+        self.name = doc.name;
+        self.stats = [];
+        doc.stats.forEach((stat) =>{
+            self.stats.push({
+                stat: new Stat(stat.stat),
+                baseStat: stat.base_stat,
+                effort: stat.effort
+            })
+        })
+        self.types = [];
+        doc.types.forEach((type) =>{
+            self.types.push(new Type(type.slot, type.type))
+        });
     }
 }
+
+module.exports = GenericPokemonDB;
