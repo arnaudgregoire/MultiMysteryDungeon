b7520b5d4f512a8c34a5ee2d2a24710a1c423f4a
arnaudgregoire <arnaudgregoire92@gmail.com>
2019-08-05 23:25:46 +0200

    implement berries

--- app/type/MdoFactory.js
+++ app/type/MdoFactory.js
@@ -1,5 +1,6 @@
 const Food = require('./object/Food');
 const Gummi = require('./object/Gummi');
+const Berry = require('./object/Berry');
 const MDO = require('./enums').MDO;
 
 class MdoFactory{
@@ -7,20 +8,38 @@ class MdoFactory{
     {
         switch (type) {
             case MDO.APPLE:
-                return new Food.Apple(x, y, type);
+                return new Food.Apple(x, y);
         
             case MDO.GOLDEN_APPLE:
-                return new Food.GoldenApple(x,y, type);
+                return new Food.GoldenApple(x,y);
             
             case MDO.GRIMY_FOOD:
-                return new Food.GrimyFood(x, y, type);
+                return new Food.GrimyFood(x, y);
 
             case MDO.RED_GUMMI:
-                return new Gummi.RedGummi(x, y, type);
+                return new Gummi.RedGummi(x, y);
             
             case MDO.SILVER_GUMMI:
-                return new Gummi.SilverGummi(x, y, type);
+                return new Gummi.SilverGummi(x, y);
+            
+            case MDO.CHERI_BERRY:
+                return new Berry.CheriBerry(x, y);
+            
+            case MDO.CHESTO_BERRY:
+                return new Berry.ChestoBerry(x, y);
 
+            case MDO.ORAN_BERRY:
+                return new Berry.OranBerry(x, y);
+
+            case MDO.PECHA_BERRY:
+                return new Berry.PechaBerry(x, y);
+
+            case MDO.RAWST_BERRY:
+                return new Berry.RawstBerry(x, y);
+
+            case MDO.SITRUS_BERRY:
+                return new Berry.SitrusBerry(x, y);
+                
             default:
                 return new Error('Object type not found');
         }
---
+++ app/type/object/Berry.js
@@ -0,0 +1,224 @@
+const Edible = require('./Edible');
+const MDO = require('../enums').MDO;
+const MDO_TILESET = require('../enums').MDO_TILESET;
+const ENUM_STATUS = require('../enums').ENUM_STATUS;
+
+/**
+ * Abstract class for all kind of berries
+ */
+class Berry extends Edible
+{
+    constructor(x, y, type, name, description, look)
+    {
+        super(x, y, type, name, description, look, 5);
+    }
+}
+
+/**
+ * Abstract class for kind of berries that heals status
+ */
+class StatusBerry extends Berry
+{
+    constructor(x, y, type, name, description, look)
+    {
+        super(x, y, type, name, description, look);
+    }
+
+    consume(player, healStatus)
+    {
+        super.consume(player);
+        if(player.status == healStatus)
+        {
+            player.status = ENUM_STATUS.NORMAL;
+        }
+    }
+}
+
+/**
+ * Abstract class for kind of berries that heals hp
+ */
+class HealBerry extends Berry
+{
+    constructor(x, y, type, name, description, look)
+    {
+        super(x, y, type, name, description, look);
+    }
+
+    consume(player, healValue)
+    {
+        super.consume(player);
+        // TODO implement healing
+    }
+}
+
+/**
+ * Real Class implementation for object CHERI_BERRY
+ */
+class CheriBerry extends StatusBerry
+{
+    /**
+     * 
+     * @param {integer} x x position on map 
+     * @param {integer} y y position on map
+     */
+    constructor(x, y)
+    {
+        super(
+            x
+            , y
+            , MDO.CHERI_BERRY
+            , "Cheri Berry"
+            , "A food item that heals the Pokémon from paralysis. It also slightly fills the Pokémon's Belly."
+            , MDO_TILESET.CHERI_BERRY
+        )
+    }
+
+    consume(player)
+    {
+        super.consume(player, ENUM_STATUS.PARALYSIS);
+    }
+}
+
+/**
+ * Real Class implementation for object CHESTO_BERRY
+ */
+class ChestoBerry extends StatusBerry
+{
+    /**
+     * 
+     * @param {integer} x x position on map 
+     * @param {integer} y y position on map
+     */
+    constructor(x, y)
+    {
+        super(
+            x
+            , y
+            , MDO.CHESTO_BERRY
+            , "Chesto Berry"
+            , "A food item that heals the Pokémon from sleep. It also slightly fills the Pokémon's Belly."
+            , MDO_TILESET.CHESTO_BERRY
+        )
+    }
+
+    consume(player)
+    {
+        super.consume(player, ENUM_STATUS.SLEEP);
+    }
+}
+
+/**
+ * Real Class implementation for object PECHA_BERRY
+ */
+class PechaBerry extends StatusBerry
+{
+    /**
+     * 
+     * @param {integer} x x position on map 
+     * @param {integer} y y position on map
+     */
+    constructor(x, y)
+    {
+        super(
+            x
+            , y
+            , MDO.PECHA_BERRY
+            , "Pecha Berry"
+            , "A food item that heals the Pokémon from poison. It also slightly fills the Pokémon's Belly."
+            , MDO_TILESET.PECHA_BERRY
+        )
+    }
+
+    consume(player)
+    {
+        super.consume(player, ENUM_STATUS.POISON);
+    }
+}
+
+/**
+ * Real Class implementation for object RAWST_BERRY
+ */
+class RawstBerry extends StatusBerry
+{
+    /**
+     * 
+     * @param {integer} x x position on map 
+     * @param {integer} y y position on map
+     */
+    constructor(x, y)
+    {
+        super(
+            x
+            , y
+            , MDO.RAWST_BERRY
+            , "Rawst Berry"
+            , "A food item that heals the Pokémon from burn. It also slightly fills the Pokémon's Belly."
+            , MDO_TILESET.RAWST_BERRY
+        )
+    }
+
+    consume(player)
+    {
+        super.consume(player, ENUM_STATUS.BURN);
+    }
+}
+
+/**
+ * Real Class implementation for object ORAN_BERRY
+ */
+class OranBerry extends HealBerry
+{
+    /**
+     * 
+     * @param {integer} x x position on map 
+     * @param {integer} y y position on map
+     */
+    constructor(x, y)
+    {
+        super(
+            x
+            , y
+            , MDO.ORAN_BERRY
+            , "Oran Berry"
+            , "A food item that heals 30 hp. It also slightly fills the Pokémon's Belly."
+            , MDO_TILESET.ORAN_BERRY
+        )
+    }
+
+    consume(player)
+    {
+        super.consume(player, 30);
+    }
+}
+
+/**
+ * Real Class implementation for object SITRUS_BERRY
+ */
+class SitrusBerry extends HealBerry
+{
+    /**
+     * 
+     * @param {integer} x x position on map 
+     * @param {integer} y y position on map
+     */
+    constructor(x, y)
+    {
+        super(
+            x
+            , y
+            , MDO.SITRUS_BERRY
+            , "Sitrus Berry"
+            , "A food item that heals 100 hp. It also slightly fills the Pokémon's Belly."
+            , MDO_TILESET.SITRUS_BERRY
+        )
+    }
+
+    consume(player)
+    {
+        super.consume(player, 100);
+    }
+}
+
+module.exports={CheriBerry, ChestoBerry, OranBerry, PechaBerry, RawstBerry, SitrusBerry}
+
+
